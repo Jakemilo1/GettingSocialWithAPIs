b51abdc3d7769761c7b40d588405345340cafcff
Jacob Milojkovich <jacobmilojkovich@jacobs-mbp.mynetworksettings.com>
2023-06-13 20:23:10 -0400

    seeds

--- utils/seed.js
+++ utils/seed.js
@@ -1,51 +1,53 @@
-const connection = require('../config/connection');
-const { Course, Student } = require('../models');
-const { getRandomName, getRandomAssignments } = require('./data');
+const mongoose = require('mongoose');
+const db = require('../models');
 
-connection.on('error', (err) => err);
-
-connection.once('open', async () => {
-  console.log('connected');
-
-  // Drop existing courses
-  await Course.deleteMany({});
-
-  // Drop existing students
-  await Student.deleteMany({});
-
-  // Create empty array to hold the students
-  const students = [];
-
-  // Loop 20 times -- add students to the students array
-  for (let i = 0; i < 20; i++) {
-    // Get some random assignment objects using a helper function that we imported from ./data
-    const assignments = getRandomAssignments(20);
-
-    const fullName = getRandomName();
-    const first = fullName.split(' ')[0];
-    const last = fullName.split(' ')[1];
-    const github = `${first}${Math.floor(Math.random() * (99 - 18 + 1) + 18)}`;
-
-    students.push({
-      first,
-      last,
-      github,
-      assignments,
-    });
-  }
-
-  // Add students to the collection and await the results
-  await Student.collection.insertMany(students);
-
-  // Add courses to the collection and await the results
-  await Course.collection.insertOne({
-    courseName: 'UCLA',
-    inPerson: false,
-    students: [...students],
-  });
-
-  // Log out the seed data to indicate what should appear in the database
-  console.table(students);
-  console.info('Seeding complete! 🌱');
-  process.exit(0);
+mongoose.connect('mongodb://localhost/socialnetworkapi', {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
+
+const userSeed = [
+  {
+    username: 'user1',
+    email: 'user1@example.com',
+    thoughts: [],
+    friends: []
+  },
+  {
+    username: 'user2',
+    email: 'user2@example.com',
+    thoughts: [],
+    friends: []
+  }
+];
+
+const thoughtSeed = [
+  {
+    thoughtText: 'What a beautiful day!',
+    username: 'user1',
+    reactions: []
+  },
+  {
+    thoughtText: 'I love coding!',
+    username: 'user2',
+    reactions: []
+  }
+];
+
+db.User
+  .deleteMany({})
+  .then(() => db.User.collection.insertMany(userSeed))
+  .then(data => {
+    console.log(data.result.n + ' user records inserted!');
+    return db.Thought.deleteMany({})
+  })
+  .then(() => db.Thought.collection.insertMany(thoughtSeed))
+  .then(data => {
+    console.log(data.result.n + ' thought records inserted!');
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
